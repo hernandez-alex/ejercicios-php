577a024906ce55b70851042828d96ce4e25aa6b5
hernandezaw <128613758+hernandez-alex@users.noreply.github.com>
2023-06-08 12:13:13 -0400

    sesion de portafolio

--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -1,3 +1,56 @@
 <?php include("cabecera.php"); ?>
-  Hola Develoteca, soy tu portafolio
+
+  <br/>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            Datos del Proyecto
+          </div>
+      
+          <div class="card-body">
+            <form action="portafolio.php" method="post">
+              Nombre del Proyecto: <input class="form-control" type="text" name="nombre" id="">
+              <br/>
+              Imagen del Proyecto: <input class="form-control" type="file" name="archivo" id="">
+              <br/>
+          
+              <input class="btn btn-success" type="submit" value="Enviar Proyecto">
+          
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Imagen</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="">
+                <td>2</td>
+                <td>Aplicación Web</td>
+                <td>imagen.jpg</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+  
+  
+
 <?php include("pie.php"); ?>
