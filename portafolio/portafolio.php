0e19d988e0a2236ebe515cb145a8474d1aaea542
alex <62507090+hernandezaw@users.noreply.github.com>
2023-06-09 06:55:29 -0400

    borrando datos

--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -9,6 +9,14 @@
     $objConexion->ejecutar($sql);
   }
 
+  if($_GET){
+    // DELETE FROM proyectos WHERE `proyectos`.`id` = 14
+    $objConexion = new conexion();
+    $id = $_GET['borrar'];
+    $sql = "DELETE FROM proyectos WHERE `proyectos`.`id` = ".$id;
+    $objConexion->ejecutar($sql);
+  }
+
   $objConexion = new conexion();
   $proyectos = $objConexion->consultar("SELECT * FROM `proyectos`");
   // print_r($proyectos);
@@ -54,11 +62,11 @@
             <tbody>
               <?php foreach($proyectos as $proyecto){?>
                 <tr class="">
-                  <td><?php echo $proyecto['id']?></td>
-                  <td><?php echo $proyecto['nombre']?></td>
-                  <td><?php echo $proyecto['imagen']?></td>
-                  <td><?php echo $proyecto['descripcion']?></td>
-                  <td><a href="#" class="btn btn-danger">Eliminar</a></td>
+                  <td><?php echo $proyecto['id']; ?></td>
+                  <td><?php echo $proyecto['nombre']; ?></td>
+                  <td><?php echo $proyecto['imagen']; ?></td>
+                  <td><?php echo $proyecto['descripcion']; ?></td>
+                  <td><a href="?borrar=<?php echo $proyecto['id']; ?>" class="btn btn-danger">Eliminar</a></td>
                 </tr>
               <?php }?>
             </tbody>
